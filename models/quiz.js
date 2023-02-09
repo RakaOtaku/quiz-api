4a58aaf150824bd0c9a06642e0c68005b32c135e
FIRST_NAME LAST_NAME <xhuagf299@gmail.com>
2023-02-09 11:15:58 +0700

    first commit

---
+++ models/quiz.js
@@ -0,0 +1,33 @@
+module.exports = (sequelize, Sequelize) => {
+    const Quiz = sequelize.define('quiz', {
+        quiz: {
+            type: Sequelize.STRING,
+        },
+        image: {
+            type: Sequelize.STRING,
+        },
+        a: {
+            type: Sequelize.STRING,
+        },
+        b: {
+            type: Sequelize.STRING,
+        },
+        c: {
+            type: Sequelize.STRING, 
+        },
+        d: {
+            type: Sequelize.STRING,
+        },
+        key: {
+            type: Sequelize.STRING,
+        },
+        categoryId: {
+            type: Sequelize.INTEGER,
+        },
+        levelId: {
+            type: Sequelize.INTEGER,
+        },
+
+    });
+    return Quiz;
+}
